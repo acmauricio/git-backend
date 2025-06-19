5d9bc0ce7615e5316fd9205edc2bf84e97470e39
mauricio alba <albamauricio.7580@gmail.com>
2025-06-18 20:46:00 -0400

    add server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/hello', (req, res)=>{
+    res.json({
+        message: 'Hola Mundo'
+    })
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ', PORT)
+})
