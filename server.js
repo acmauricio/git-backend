2a74aed3f79d5da5e5ca25899f76ab23061c94f2
mauricio alba <albamauricio.7580@gmail.com>
2025-06-18 20:49:43 -0400

    add status in server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 app.get('/hello', (req, res)=>{
     res.json({
         message: 'Hola Mundo'
+    })
+})
+
+message = `SERVICE IS RUNNING ON PORT ${PORT}`
+
+app.get('/status', (req, res)=>{
+    res.json({
+        message: message
     })
 })
 
