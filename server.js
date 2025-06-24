dd627352ef02240917ac6c63ed1532cc6677686e
mauricio alba <mauricioalba.060501@gmail.com>
2025-06-23 21:15:37 -0400

    matt4

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
+// matt4
+
 app.get('/', (req, res)=>{
     res.json({
         message: 'Welcome to my api'
